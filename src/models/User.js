5943f78226110731d0d411cef06ae96d0f239079
Tiago <tiagoprysthon14@gmail.com>
2023-01-26 15:58:30 -0300

    feat: User model created

---
+++ src/models/User.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users'
+    }
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId', as: 'posts'
+    });
+  };
+
+  return User;
+};
