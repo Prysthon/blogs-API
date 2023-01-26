9a1fab99927a55ed26e1fc698e1a57ab81cc2b01
Tiago <tiagoprysthon14@gmail.com>
2023-01-26 15:47:08 -0300

    feat: migrations created

---
+++ src/migrations/20230126182017-blog_posts.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('blog_posts', {
+       id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+       title: Sequelize.STRING,
+       content: Sequelize.STRING,
+       user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+       published: Sequelize.DATE,
+       updated: Sequelize.DATE,
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
