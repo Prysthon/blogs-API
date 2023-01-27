f7720ecaaaa718fb9b1fa73f02898578d16ffc34
Tiago <tiagoprysthon14@gmail.com>
2023-01-27 14:37:06 -0300

    feat: blogPost model

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-      id: { type: DataTypes.INTEGER, primaryKey: true },
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
@@ -13,11 +13,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
 
-  // User.associate = (models) => {
-  //   User.hasMany(models.BlogPosts, {
-  //     foreignKey: 'userId', as: 'posts'
-  //   });
-  // };
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, 
+      { foreignKey: 'userId', as: 'posts' });
+  };
 
   return User;
 };
